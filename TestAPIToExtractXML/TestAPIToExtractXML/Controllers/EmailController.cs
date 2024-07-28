dccd1e7bdc2b3f68180f818f2b1b6acd5c0bfe67
aryadeepth <102133931+aryadeepth@users.noreply.github.com>
2024-07-28 19:44:41 +0530

    Push Test Application
    Push Test Application

---
+++ TestAPIToExtractXML/TestAPIToExtractXML/Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Http;
+using TestAPIToExtractXML.Repository;
+using TestAPIToExtractXML.Model;
+
+namespace TestAPIToExtractXML.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Email_GetToExtract(String email)
+        {
+            EmailRepository emr = new EmailRepository();
+            if (emr.ValidateMessage(email))
+            {
+                //fetch extracted data after calculation
+                ExtraxtedData data =  emr.ExtractEmailContent(email);
+                if (data.CostCentre == "UNKNOWN")
+                {
+                    return BadRequest(data);
+                }
+                else
+                {
+                    return Ok(data);
+                }
+            }
+            else
+            {              
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ TestAPIToExtractXML/TestAPIToExtractXML/Repository/EmailRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using TestAPIToExtractXML.Model;
+
+namespace TestAPIToExtractXML.Repository
+{
+    public class EmailRepository
+    {
+        public Boolean ValidateMessage(string message)
+        {
+            try
+            {
+                XmlDocument xdoc = new XmlDocument();
+                xdoc.LoadXml(message);
+                return true;
+            }
+            catch (XmlException xe)
+            {
+                return false;
+            }
+        }
+        public ExtraxtedData ExtractEmailContent(string data)
+        {
+            ExtraxtedData exData = new ExtraxtedData();
+            var rootElement = XElement.Parse(data);
+            var total = rootElement.Element("total").Value;
+            var costCentre = rootElement.Element("cost_centre").Value;
+            if (costCentre != null)
+            {
+                var salesTaxRate = exData.SalesTaxRate;
+                //using reverse sales tax calculation formula, list price = total ÷ (1 + tax rate)
+                var listPrice = Convert.ToDouble(total) / (1 + Convert.ToDouble(salesTaxRate));
+                var salesTaxAmount = Convert.ToDouble(listPrice) * Convert.ToDouble(salesTaxRate);
+                exData.TotalExcludingTax = listPrice;
+                exData.SalesTaxAmount = salesTaxAmount;
+                exData.CostCentre = costCentre;
+            }
+            else
+            {
+                exData.CostCentre = "UNKNOWN";
+            }
+            return exData;
+        }
+    }
+}
